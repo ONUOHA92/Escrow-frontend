b92385278489d4f0b00b04cc3ae086550f411ea7
Onuoha Collins <onuohacollinschidubem@gmail.com>
2024-03-12 14:45:10 +0100

    fit slow react-scroll

--- src/components/ui/organisms/whyescrowPro/team/index.tsx
+++ src/components/ui/organisms/whyescrowPro/team/index.tsx
@@ -19,7 +19,7 @@ const teams = [
   },
   {
     name: "Willans Alexander",
-    role: "Team Lead Developer",
+    role: "Team Lead",
     imageUrl: "../../assets/team/teamLead.jpeg",
     bio: "Ultricies massa malesuada viverra cras lobortis. Tempor orci hac ligula dapibus mauris sit ut eu. Eget turpis urna maecenas cras. Nisl dictum.",
     xUrl: "#",
